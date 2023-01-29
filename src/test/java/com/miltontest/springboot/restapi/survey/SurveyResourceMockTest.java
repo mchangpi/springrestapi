412722595000c0ff80ddd497f1a15d27f6a02286
Milton Chang <mchangpi@gmail.com>
2023-01-29 20:03:24 +0800

    L114 Writing Spring Boot Mock MVC Unit Test for POST Method

--- src/test/java/com/miltontest/springboot/restapi/survey/SurveyResourceMockTest.java
+++ src/test/java/com/miltontest/springboot/restapi/survey/SurveyResourceMockTest.java
@@ -1,6 +1,9 @@
 package com.miltontest.springboot.restapi.survey;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -27,6 +31,7 @@ public class SurveyResourceMockTest {
   private MockMvc mockMvc;
   
   private static String SPECIFIC_REQUEST = "http://localhost:8080/surveys/1/questions/1";
+  private static String GENERIC_REQUEST = "http://localhost:8080/surveys/1/questions";
   
   @Test
   void getSpecificQuestion_Status404() throws Exception {
@@ -62,5 +67,35 @@ public class SurveyResourceMockTest {
         {"id":"1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"answer":"AWS"}
         """;
     JSONAssert.assertEquals(expected, mvcResult.getResponse().getContentAsString(), false);
+  }
+  
+  @Test
+  void addQuestion_Status201() throws Exception{
+    String requestBody = """
+        {
+          "description": "Your Favorite Language",
+          "options": [
+            "Java",
+            "Python",
+            "JavaScript",
+            "Haskell"
+          ],
+          "answer": "Python"
+        }
+      """;
+    
+    when(surveyService.addQuestion(anyString(), any())).thenReturn("SOME_ID");
+
+    RequestBuilder requestBuilder = 
+        MockMvcRequestBuilders.post(GENERIC_REQUEST)
+        .accept(MediaType.APPLICATION_JSON)
+        .content(requestBody).contentType(MediaType.APPLICATION_JSON);
+
+    MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();    
+    
+    String locationHeader = mvcResult.getResponse().getHeader("Location");
+    
+    assertEquals(201, mvcResult.getResponse().getStatus());
+    assertTrue(locationHeader.contains("/surveys/1/questions/SOME_ID"));
   }
 }
