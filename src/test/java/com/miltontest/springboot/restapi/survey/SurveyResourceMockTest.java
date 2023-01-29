68ad24c5481d124a5a818a787f5e5aa4c2e5fc60
Milton Chang <mchangpi@gmail.com>
2023-01-29 18:01:56 +0800

    L113 Improve Asserts for Spring Boot Mock MVC Unit Test

--- src/test/java/com/miltontest/springboot/restapi/survey/SurveyResourceMockTest.java
+++ src/test/java/com/miltontest/springboot/restapi/survey/SurveyResourceMockTest.java
@@ -1,6 +1,12 @@
 package com.miltontest.springboot.restapi.survey;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -31,16 +37,30 @@ public class SurveyResourceMockTest {
     
     System.out.println(mvcResult.getResponse().getContentAsString());
     System.out.println(mvcResult.getResponse().getStatus());
+    
+    assertEquals(404, mvcResult.getResponse().getStatus());
   }
   
   @Test
-  void getSpecificQuestion_Status404() throws Exception {
+  void getSpecificQuestion_Status200() throws Exception {
     RequestBuilder requestBuilder = 
         MockMvcRequestBuilders.get(SPECIFIC_REQUEST).accept(MediaType.APPLICATION_JSON);
     
+    Question question = new Question("1", "Most Popular Cloud Platform Today",
+        Arrays.asList("AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+    when(surveyService.getSpecificQuestion("1", "1")).thenReturn(question);
+    
+    //Mock SurveyService and Test against SurveyResource!
     MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
     
     System.out.println(mvcResult.getResponse().getContentAsString());
     System.out.println(mvcResult.getResponse().getStatus());
+    
+    assertEquals(200, mvcResult.getResponse().getStatus());
+    
+    String expected = """
+        {"id":"1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"answer":"AWS"}
+        """;
+    JSONAssert.assertEquals(expected, mvcResult.getResponse().getContentAsString(), false);
   }
 }
