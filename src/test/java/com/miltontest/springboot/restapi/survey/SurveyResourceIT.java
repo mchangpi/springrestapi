3d6a033b2f3f223bc3abdc50562fcb0b5bc8a026
Milton Chang <mchangpi@gmail.com>
2023-01-29 13:44:40 +0800

    L107 Writing Asserts for JSON in Spring BootTests - JsonAssert

---
+++ src/test/java/com/miltontest/springboot/restapi/survey/SurveyResourceIT.java
@@ -0,0 +1,40 @@
+package com.miltontest.springboot.restapi.survey;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SurveyResourceIT {
+
+  private static String request = "/surveys/1/questions/2";
+
+  @Autowired
+  TestRestTemplate template;
+  
+  @Test
+  void getSpecificQuestion_JsonAssert() throws JSONException {
+    ResponseEntity<String> response = template.getForEntity(request, String.class);
+    //System.out.println(response.getHeaders());
+    //System.out.println(response.getBody());
+  
+    String expected = 
+        """
+        {   
+            "id":"2",
+            "description":"Fastest Growing Cloud Platform",
+            "options":["AWS","Azure","Google Cloud","Oracle Cloud"]
+        }    
+        """;
+
+    //assertEquals(expected.trim(), response.getBody());
+    JSONAssert.assertEquals(expected, response.getBody(), false);
+  }
+}
